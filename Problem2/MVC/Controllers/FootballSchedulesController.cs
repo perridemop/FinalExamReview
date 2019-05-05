025cf45668cec92822d3e63e703db64a2db0f3e1
Perri Demopulos <perridemop@gmail.com>
2019-05-05 18:26:59 -0500

    MVC Started

---
+++ Problem2/MVC/Controllers/FootballSchedulesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class FootballSchedulesController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: FootballSchedules
+        public ActionResult Index()
+        {
+            return View(db.FootballSchedules.ToList());
+        }
+
+ 
+    }
+}
---
+++ Problem2/MVC/Controllers/ScheduleController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC;
+
+namespace MVC.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            return View(db.FootballSchedules.ToList());
+        }
+
+        // GET: Schedule/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FootballSchedule footballSchedule = db.FootballSchedules.Find(id);
+            if (footballSchedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(footballSchedule);
+        }
+
+        // GET: Schedule/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FootballSchedule footballSchedule = db.FootballSchedules.Find(id);
+            if (footballSchedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(footballSchedule);
+        }
+
+        // POST: Schedule/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Season,Opponent,Date,IsHomeGame")] FootballSchedule footballSchedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(footballSchedule).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(footballSchedule);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
